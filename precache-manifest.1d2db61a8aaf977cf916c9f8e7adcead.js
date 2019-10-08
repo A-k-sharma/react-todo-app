4e869a50bbf12cc3a181401dcfbec9c4bd18aaeb
Ashu <ashu.sharma@tothenew.com>
2019-10-09 00:06:16 +0530

    Updates

---
+++ precache-manifest.1d2db61a8aaf977cf916c9f8e7adcead.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "edfd61088144b5519004e1a465affcd4",
+    "url": "/react-todo-app/static/media/bg.edfd6108.jpg"
+  },
+  {
+    "revision": "b1aeed9f6344163e5e40",
+    "url": "/react-todo-app/static/js/runtime~main.a3a648b2.js"
+  },
+  {
+    "revision": "06f6e381ee4762fbf4f3",
+    "url": "/react-todo-app/static/js/main.4a109288.chunk.js"
+  },
+  {
+    "revision": "c249997862f2cbb00cd6",
+    "url": "/react-todo-app/static/js/2.ee57e6ca.chunk.js"
+  },
+  {
+    "revision": "06f6e381ee4762fbf4f3",
+    "url": "/react-todo-app/static/css/main.5484524a.chunk.css"
+  },
+  {
+    "revision": "e85e87c024e8ee32064968ce00d71e7f",
+    "url": "/react-todo-app/index.html"
+  }
+];
